a4ffd7c546bd52dafd64534ac419005717fc4869
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-22 17:48:03 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function inputHandler(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type='password' onChange={inputHandler}/> 
+        </div>
     )
 }
 
